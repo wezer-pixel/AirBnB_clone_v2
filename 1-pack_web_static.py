72583401ecbe25f8411535419abc7d97ec60b6ae
Wezer100 <76890104+wezer-pixel@users.noreply.github.com>
2023-10-06 12:51:25 +0300

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,30 +1,21 @@
-#!/usr/bin/python3
 from fabric.api import local
 from datetime import datetime
-import os
 
 def do_pack():
-    """
-    Create a compressed archive from the contents of the web_static folder.
-    """
-    # Create the versions folder if it doesn't exist
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
+    """Packs the contents of web_static into a .tgz archive."""
 
-    # Generate the archive filename (web_static_<year><month><day><hour><minute><second>.tgz)
-    now = datetime.utcnow()
-    archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+    # Create the versions folder if it doesn't exist
+    local("mkdir -p versions")
+
+    # Generate the archive name with the current date and time
+    now = datetime.now()
+    archive_name = "web_static_{:04}{:02}{:02}{:02}{:02}{:02}.tgz".format(
         now.year, now.month, now.day, now.hour, now.minute, now.second)
 
-    # Create the archive command
-    archive_command = "tar -cvzf versions/{} web_static".format(archive_name)
+    # Create the .tgz archive
+    result = local("tar -cvzf versions/{} web_static".format(archive_name))
 
-    # Run the archive command
-    local_result = local(archive_command)
-
-    # Check if the command was successful
-    if local_result.failed:
-        return None
+    if result.succeeded:
+        return "versions/{}".format(archive_name)
     else:
-        return os.path.join("versions", archive_name)
-
+        return None
